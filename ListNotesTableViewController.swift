60331b651be211069c8f21f588e6ed928c64a4a9
Himanshu Bhadoria <himan2016@gmail.com>
2016-07-29 12:54:19 -0700

    init

---
+++ ListNotesTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListNotesTableViewController.swift
+//  EasyPlan
+//
+//  Created by Himanshu Bhadoria on 7/29/16.
+//  Copyright © 2016 HBhadoria. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListNotesTableViewController: UITableViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // 1
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // 2
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 3
+        let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath)
+        
+        // 4
+        cell.textLabel?.text = "Yay - it's working!"
+        
+        // 5
+        return cell
+    }
+    
+}
